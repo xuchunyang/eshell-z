48e8b9e93226979b2f32367a8d2c04ecdb40644b
Chunyang Xu <xuchunyang56@gmail.com>
2015-07-25 20:03:47 +0800

    Revert "Replace seq-some-p"
    This reverts commit c8d9a49ed24ae57373c308ebbbbef724d9509703.

--- eshell-z.el
+++ eshell-z.el
@@ -183,13 +183,11 @@ If it is nil, the freq-dir-hash-table will not be written to disk."
              ;; $HOME isn't worth matching
              (string= current-directory (eshell-z--expand-directory-name "~"))
              ;; don't track excluded directory trees
-             (catch 'seq--break
-               (seq-doseq (root eshell-z-exclude-dirs)
-                 (when (and (stringp root)
-                            (eshell-z--directory-within-p
-                             root current-directory))
-                   (throw 'seq--break t)))
-               nil))
+             (seq-some-p (lambda (root)
+                           (and (stringp root)
+                                (eshell-z--directory-within-p
+                                 root current-directory)))
+                         eshell-z-exclude-dirs))
       (let* (
              ;; Remove end slash, z doesn't use it
              (key current-directory)
